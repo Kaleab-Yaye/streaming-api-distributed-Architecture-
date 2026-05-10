9dd480a3b81aad780298c77fff78363824d062eb
Kaleab-Yaye <kaleabtesfaye222@gmail.com>
2026-05-10 19:06:04 +0300

    add script to push spring image to github

--- Docker/legacy/build.ps1
+++ Docker/legacy/build.ps1
@@ -2,6 +2,6 @@
 ./mvnw clean install -DskipTests
 docker-compose build --no-cache api nginx
 docker compose up -d --build
-docker tag api  ghcr.io/kaleab-yaye/api:latest
+docker tag  api  ghcr.io/kaleab-yaye/api:latest
 docker push ghcr.io/kaleab-yaye/api:latest
 
