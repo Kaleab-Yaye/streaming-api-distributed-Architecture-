58e0b328678f9b991bdbbcf3c6f03722b708d58b
Kaleab-Yaye <kaleabtesfaye222@gmail.com>
2026-07-23 12:43:57 +0300

    test code to see if every thing will work fine from a container

--- GO/main.go
+++ GO/main.go
@@ -1,2 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func HandelTestReqeust(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("requests was made")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("all good"))
+
+}
+
+func main() {
+	fmt.Println("server starting at port 3000")
+	http.HandleFunc("/test", HandelTestReqeust)
+	err := http.ListenAndServe("3000", nil)
+	if err != nil {
+		fmt.Println("there was an error with attachign the server to the port")
+
+		return
+
+	}
+}
