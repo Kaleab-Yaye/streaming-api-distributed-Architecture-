f8988c236739581ed846b3db4cb249d104ced2bb
Kaleab-Yaye <kaleabtesfaye222@gmail.com>
2026-08-07 16:49:22 +0300

    Add safeguard against double registration of the same node in DB

--- src/main/java/com/adnakiwoch/platform/streaming_api/repository/StreamingNodeRepo.java
+++ src/main/java/com/adnakiwoch/platform/streaming_api/repository/StreamingNodeRepo.java
@@ -5,9 +5,12 @@ import com.adnakiwoch.platform.streaming_api.domain.StreamingNode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface StreamingNodeRepo extends JpaRepository<StreamingNode, UUID > {
+
+    Optional<StreamingNode> getStreamingNodeByIpAddr(String ipAddr);
 
 }
--- src/main/java/com/adnakiwoch/platform/streaming_api/service/internal/GoStreamingNodeService.java
+++ src/main/java/com/adnakiwoch/platform/streaming_api/service/internal/GoStreamingNodeService.java
@@ -3,11 +3,14 @@ package com.adnakiwoch.platform.streaming_api.service.internal;
 
 import com.adnakiwoch.platform.streaming_api.domain.StreamingNode;
 import com.adnakiwoch.platform.streaming_api.dto.request.StreamingNode.StreamingNodeRegistRequest;
+import com.adnakiwoch.platform.streaming_api.exception.resource.ResourceNotFoundException;
 import com.adnakiwoch.platform.streaming_api.repository.StreamingNodeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class GoStreamingNodeService {
@@ -20,11 +23,23 @@ public class GoStreamingNodeService {
 
     public ResponseEntity<HttpStatus> liveNodeHandler (StreamingNodeRegistRequest streamingNodeRegistRequest
     ){
+
+        Optional<StreamingNode> optionalStreamingNode = streamingNodeRepo.getStreamingNodeByIpAddr(streamingNodeRegistRequest.ip_addr());
+
+        if(optionalStreamingNode.isPresent()){
+
+            return ResponseEntity.ok().build();
+
+        }
+
         StreamingNode newStreamingNode = new StreamingNode();
         newStreamingNode.setIpAddr(streamingNodeRegistRequest.ip_addr());
         newStreamingNode.setPortNumber(streamingNodeRegistRequest.port_number())
         ;
         newStreamingNode.setUpStat(true);
+
+        // lets handel if the server tries to register twice, or has crashed before and want's to regist again
+
 
         streamingNodeRepo.save(newStreamingNode);
 
